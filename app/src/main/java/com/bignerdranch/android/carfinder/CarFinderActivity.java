43baf3580dfe913cdf8f60b80b94e60847d5cf53
Michael Yang <yangmh@appstate.edu>
2016-11-20 17:41:10 -0500

    get gps current location

--- app/src/main/java/com/bignerdranch/android/carfinder/CarFinderActivity.java
+++ app/src/main/java/com/bignerdranch/android/carfinder/CarFinderActivity.java
@@ -1,13 +1,40 @@
 package com.bignerdranch.android.carfinder;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CarFinderActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+public class CarFinderActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_finder);
+    protected Fragment createFragment() {
+        return CarFinderFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if(errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GooglePlayServicesUtil
+                    .getErrorDialog(errorCode, this, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    // Leave if services are unavailable.
+                                    finish();
+                                }
+                            });
+            errorDialog.show();
+        }
     }
 }
