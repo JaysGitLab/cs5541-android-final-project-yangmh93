620f681f972952843a5005d811f0254d32141a01
Michael Yang <yangmh@appstate.edu>
2016-11-19 23:04:46 -0500

    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/carfinder/CarFinderActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.carfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarFinderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_finder);
+    }
+}
